18c030ca435f6df9ec58a58f117db201098e1129
JiaJun <jiakjoon98@gmail.com>
2023-09-28 05:09:46 +0800

    Adding AntDesign basic Navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import "./App.css";
+import { Menu } from 'antd';
+import {Route, Routes, useNavigate} from "react-router-dom";
+import { HomeOutlined, InfoOutlined, CheckOutlined } from "@ant-design/icons";
+//import "antd/dist/antd.css"
 
 function App() {
+  
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", flexDirection: "column", flex: 1, height:"100vh"}}>
+      <Header/>
+      <div style={{display: "flex", flexDirection: "row", flex: 1}}>
+        <SideMenu/>
+      </div>
+     <p>The current time is {currentTime}.</p> 
+     <Footer/>
     </div>
+        
+    
   );
 }
 
-export default App;
+function Header(){ // Testing
+  return <div style = {{
+    height:50, 
+    backgroundColor: "lightskyblue", 
+    color: "white", 
+    display:"flex", 
+    justifyContent: 'center', 
+    alignItems: "center",
+    fontWeight :"bold"}}>
+      Header
+      </div>
+}
+
+function Footer(){ //Testing
+  return <div style = {{
+    height:50, 
+    backgroundColor: "lightgray", 
+    color: "black", 
+    display:"flex", 
+    justifyContent: 'center', 
+    alignItems: "center",
+    fontWeight :"bold"}}>
+      Footer
+      </div>
+}
+
+function SideMenu(){
+  const navigate = useNavigate()
+  return <div style={{ display: 'flex', flexDirection:"row"}}>
+      <Menu
+      onClick = {({key}) =>{navigate(key)}} 
+      defaultSelectedKeys={window.location.pathname}
+      items= {
+        [{label: 'Home', key:"/", icon : <HomeOutlined/>}, 
+        {label: 'About', key:"/about", icon : <InfoOutlined/>}, //Temporary use as home page; change to moving centre later
+        {label: 'Checksum', key:"/checksum", icon: <CheckOutlined/>}]} //Temporary use as home page; change to moving centre later
+        > 
+      </Menu>
+      <div>
+        <Content/>
+        </div>
+    </div>
+}
+
+function Content(){
+  return <div>
+    <Routes>
+      <Route path = "/" element={<div>Home</div>}></Route>
+      <Route path = "/about" element={<div>About</div>}></Route>
+      <Route path = "/checksum" element={<div>Checksum</div>}></Route> 
+    </Routes>
+  </div> //Becareful of checksum might re-direct to wrong page
+}
+
+
+export default App
---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./index.css";
+import { HomeOutlined, InfoOutlined, CheckOutlined } from "@ant-design/icons";
+import type { MenuProps } from "antd";
+import { Menu } from "antd";
+
+const items: MenuProps["items"] = [
+  {
+    label: "Home",
+    key: "home",
+    icon: <HomeOutlined />
+  },
+  {
+    label: "About",
+    key: "about",
+    icon: <InfoOutlined />
+  },
+  {
+    label: "CheckSum",
+    key: "checksum",
+    icon: <CheckOutlined />
+  }
+];
+
+const App: React.FC = () => {
+  const [current, setCurrent] = useState("home");
+
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <Menu
+      onClick={onClick}
+      selectedKeys={[current]}
+      mode="horizontal"
+      items={items}
+    />
+  );
+};
+
+export default App;
